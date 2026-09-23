c6aab5b444cfdfa9ec31807853b157d5d0b87a67
mikec <mikec@users.noreply.github.com>
2026-09-23 01:22:12 -0400

    Fix buy points API import path on Vercel.
    The nested buy handler was resolving lib from api/lib and crashing checkout.

--- api/points/buy/index.js
+++ api/points/buy/index.js
@@ -1,4 +1,4 @@
-import { handlePointsBuy } from "../../lib/points-buy-handlers.js";
+import { handlePointsBuy } from "../../../lib/points-buy-handlers.js";
 
 export default function handler(req, res) {
   return handlePointsBuy(req, res);
